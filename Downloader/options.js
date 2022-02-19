43b9122522acf47e1ac3967ef803563fc386fed3
Piotr Kutowiński <piotr.kutowinski@gmail.com>
2022-02-19 23:24:05 +0100

    Adjust naming of page elements

--- Downloader/options.js
+++ Downloader/options.js
@@ -1,8 +1,5 @@
 let saveButton = document.getElementById("saveButton");
 let addButton = document.getElementById("addOptionButton")
-let address = document.getElementById("addressTextBox");
-let regexp = document.getElementById("regexpTextBox");
-let subdir = document.getElementById("subdirTextBox");
 
 saveButton.addEventListener("click", handleSaveButtonClick);
 addButton.addEventListener("click", handleAddButtonClick);
@@ -24,19 +21,19 @@ function handleRemoveButtonClick(evt) {
     console.log(`Remove button click: ${evt.currentTarget.id}`);
     console.log(`Remove button click: ${evt.currentTarget.uniqueId}`);
 
-    let optionsDiv = document.getElementById(`OptionsDiv`);
+    let optionsDiv = document.getElementById(`DonwloadEntriesDiv`);
     let optionDiv = document.getElementById(`optionDiv${evt.currentTarget.uniqueId}}`);
     optionsDiv.removeChild(optionDiv);
 }
 
 async function drawOptions(options) {
-    let masterDiv = document.getElementById("OptionsDiv");
+    let masterDiv = document.getElementById("DonwloadEntriesDiv");
 
     let now = Date.now();
 
-    let optionsDiv = document.createElement(`div`);
-    optionsDiv.id = `optionDiv${now}`;
-    optionsDiv.innerHTML = `<text>Page address: </text>
+    let downloadEntriesDiv = document.createElement(`div`);
+    downloadEntriesDiv.id = `optionDiv${now}`;
+    downloadEntriesDiv.innerHTML = `<text>Page address: </text>
         <input type="text" size="40" maxlength="40" id="addressTextBox${now}">
         <text>Save subdirectory: </text>
         <input type="text" size="40" maxlength="40" id="subdirTextBox${now}">
@@ -44,7 +41,7 @@ async function drawOptions(options) {
         <input type="text" size="40" maxlength="40" id="regexpTextBox${now}"> 
         <button id="saveButton">save</button> <button id="removeButton${now}">remove</button>`;
 
-    masterDiv.appendChild(optionsDiv);
+    masterDiv.appendChild(downloadEntriesDiv);
 
     document.getElementById(`addressTextBox${now}`).value = options.PageAddress;
     document.getElementById(`subdirTextBox${now}`).value = options.DownloadSubDirectory;
@@ -61,8 +58,11 @@ async function Initialize() {
         downloadSettings.forEach(setting => {
             drawOptions(setting);
         });
-    } else {
-        drawOptions(downloadSettings)
+    } else if (downloadSettings) {
+        drawOptions(downloadSettings);
+    }
+    else {
+        drawOptions(new DownloadEntry("", "", ""));
     }
 }
 
