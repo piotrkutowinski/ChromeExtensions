c63efd194ddd11b64bbf79bac2cf58ef2f28bab5
Piotr Kutowiński <piotr.kutowinski@gmail.com>
2022-02-20 01:10:19 +0100

    background.js - use all download entries
    Updated onDeterminingFilename listener to use an array of download entries and select only the one that matches current url.

--- Downloader/background.js
+++ Downloader/background.js
@@ -1,45 +1,52 @@
-const downloadSettings = {};
+let downloadSettings = {};
 
 chrome.downloads.onDeterminingFilename.addListener((downloadItem, suggest) => {
-    let downloadSetting = {};
-
     console.log(`Downloading file: ${downloadItem.filename}`);
     console.log(`Download settings: ${downloadSettings}`);
-
-    // temp WA for not finnished work.
-    downloadSetting = downloadSettings[0];
-    
-    console.log('Page address pattern is ' + downloadSetting.PageAddress);
-    console.log('Save subdirectory is ' + downloadSetting.DownloadSubDirectory);
-    console.log('Save directory regexp is ' + downloadSetting.SaveDirRegexp);
 
     getCurrentTabUrl().then((url) => {
         console.log(`Tab url: "${url}"`);
 
-        if (url.includes(downloadSetting.PageAddress)) {
-            console.log("Hsd url");
-
-            let regex = new RegExp(downloadSetting.SaveDirRegexp);
-            let m = url.match(regex);
-
-            if (m != null) {
-                let filePath = `${m[0]}\\${downloadItem.filename}`;
-                
-                // Tests undefined, null or empty. If sub dir is not empty then append to path.
-                if (downloadSetting.DownloadSubDirectory)
-                {
-                    filePath = `${downloadSetting.DownloadSubDirectory}\\${filePath}`
-                }
-
-                console.log(`Saving as: ${filePath}`);
-
-                suggest({ filename: `${filePath}` });
+        let downloadSetting = {};
+        for (var i = 0; i < downloadSettings.length; i++) {
+            if (url.includes(downloadSettings[i].PageAddress)) {
+                downloadSetting = downloadSettings[i];
+                break;
             }
         }
+
+        console.log('Page address pattern is ' + downloadSetting.PageAddress);
+        console.log('Save subdirectory is ' + downloadSetting.DownloadSubDirectory);
+        console.log('Save directory regexp is ' + downloadSetting.SaveDirRegexp);
+
+        let filePath = updateFilePath(downloadItem.filename, url, downloadSetting);
+
+        console.log(`Saving as: ${filePath}`);
+        suggest({ filename: `${filePath}` });
     });
 
     return true;
 });
+
+function updateFilePath(_fileName, _url, _downloadSettings) {
+    let regex = new RegExp(_downloadSettings.SaveDirRegexp);
+    let m = _url.match(regex);
+
+    let filePath = "";
+    if (m != null && m.length > 0) {
+        filePath = `${m[0]}\\${_fileName}`;
+
+        // Tests undefined, null or empty. If sub dir is not empty then append to path.
+        if (_downloadSettings.DownloadSubDirectory) {
+            filePath = `${_downloadSettings.DownloadSubDirectory}\\${filePath}`
+        }
+    }
+    else {
+        filePath = _fileName
+    }
+
+    return filePath;
+}
 
 async function getCurrentTabUrl() {
     let queryOptions = { active: true, currentWindow: true };
@@ -50,7 +57,8 @@ async function getCurrentTabUrl() {
 
 async function loadStoredDownloadSettings() {
     await getLocalStorageValue("downloadSettings").then((options) => {
-        Object.assign(downloadSettings, options.downloadSettings);
+        // awkward conver object to array.
+        downloadSettings =  Object.values(options.downloadSettings);
         console.log(`Download settings loaded.`);
     });
 
